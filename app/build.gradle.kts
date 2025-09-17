754d4fea306787e502a88b2f5e815ac5ac742d87
doselillo <doselfurioso@gmail.com>
2025-09-17 13:39:35 +0200

    version number change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.doselfurioso.musvisto"
         minSdk = 29
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.0-beta2"
+        versionCode = 3
+        versionName = "1.0.0-beta3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
