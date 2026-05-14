b293dec47f79bafabf306e3c7f95d64b5a574eed
doselillo <doselfurioso@gmail.com>
2026-05-14 13:35:38 +0200

    chore: bump versionName a 1.0.0-beta4 (versionCode 5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.doselfurioso.musvisto"
         minSdk = 29
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.0-beta3"
+        versionCode = 5
+        versionName = "1.0.0-beta4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
